ca79cdc15b714fecc67936e5bedcebc2818daeb3
danielhama <danielhamamura@hotmail.com>
2023-08-16 00:14:34 -0300

    Após implementar o comparable utilizando o arnkng como parametro
    Signed-off-by: danielhama <danielhamamura@hotmail.com>

--- src/Resolucao/java/comparableXcomparator/baeldung/com/java/comparator/comparable/Player.java
+++ src/Resolucao/java/comparableXcomparator/baeldung/com/java/comparator/comparable/Player.java
@@ -1,6 +1,19 @@
 package Resolucao.java.comparableXcomparator.baeldung.com.java.comparator.comparable;
 
-public class Player {
+//public class Player {
+//    private int ranking;
+//    private String name;
+//    private int age;
+//
+//    public Player(int ranking, String name, int age) {
+//        this.ranking = ranking;
+//        this.name = name;
+//        this.age = age;
+//    }
+
+// Comparable
+
+public class Player implements Comparable<Player> {
     private int ranking;
     private String name;
     private int age;
@@ -11,26 +24,13 @@ public class Player {
         this.age = age;
     }
 
-// Comparable
+    @Override
+    public int compareTo(Player player) {
+        return Integer.compare(this.ranking, player.ranking);
+    }
 
-//public class Player implements Comparable<Player> {
-//    private int ranking;
-//    private String name;
-//    private int age;
-//
-//    public Player(int ranking, String name, int age) {
-//        this.ranking = ranking;
-//        this.name = name;
-//        this.age = age;
-//    }
-//
-//    @Override
-//    public int compareTo(Player player) {
-//        return Integer.compare(this.ranking, player.ranking);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Player{" + "ranking=" + ranking + ", name=" + name + ", age=" + age + '}';
-//    }
+    @Override
+    public String toString() {
+        return "Player{" + "ranking=" + ranking + ", name=" + name + ", age=" + age + '}';
+    }
 }
