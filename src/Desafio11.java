841c4d9363f09aa72b2e2a228b63c0997c20dd95
danielhama <danielhamamura@hotmail.com>
2023-08-22 19:56:15 -0300

    Desafio11
    Signed-off-by: danielhama <danielhamamura@hotmail.com>

---
+++ src/Desafio11.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Desafio 11 - Encontre a soma dos quadrados de todos os números da lista:
+//        Utilizando a Stream API, encontre a soma dos quadrados de todos
+//        os números da lista e exiba o resultado no console.
+public class Desafio11 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+
+        Integer soma = numeros.stream()
+                .mapToInt(s -> s * s)
+                .sum();
+        System.out.println(soma);
+    }
+}
