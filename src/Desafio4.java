4e23b16358804195d6e5054a44a226e5bfca23df
danielhama <danielhamamura@hotmail.com>
2023-08-22 19:44:13 -0300

    Desafio8
    Signed-off-by: danielhama <danielhamamura@hotmail.com>

--- src/Desafio4.java
+++ src/Desafio4.java
@@ -10,11 +10,11 @@ import java.util.List;
 public class Desafio4 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        List<Integer> numeros2 = new ArrayList<>(numeros);
+        numeros2.removeIf(s -> s % 2 != 0);
 
-//        numeros.stream()
-//                .filter(s -> s % 2 != 0)
-//                .mapToInt(Integer::intValue)
-//                .forEach(numeros::remove);
+        System.out.println(numeros2);
+
 
         List<Integer> impares = new ArrayList<>();
         numeros.stream()
---
+++ src/Desafio8.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio8 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> digitos = new ArrayList<>();
+
+        numeros.stream().map(s -> s.toString())
+//                        .map(String::length)
+                .forEach(s -> {
+                    if (s.length() == 1) {
+                        digitos.add(Integer.parseInt(s));
+                    } else {
+                        for (int i = 0; i < s.length(); i++) {
+                            digitos.add(Integer.parseInt(s.substring(i, i + 1)));
+                        }
+                    }
+                });
+        Integer somadigitos = digitos.stream().mapToInt(s -> s).sum();
+        System.out.println(somadigitos);
+        System.out.println(numeros.stream().mapToInt(s -> s).sum());
+
+
+    }
+}
