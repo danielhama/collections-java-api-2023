ac8aca5a6df883c97ff641bbb6a94db6b7d8b3dd
danielhama <danielhamamura@hotmail.com>
2023-08-22 17:52:52 -0300

    Desafio1
    Signed-off-by: danielhama <danielhamamura@hotmail.com>

---
+++ src/Desafio4.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Desafio 4 - Remova todos os valores ímpares:
+//        Utilize a Stream API para remover os
+//        valores ímpares da lista e imprima a
+//        lista resultante no console.
+
+public class Desafio4 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+//        numeros.stream()
+//                .filter(s -> s % 2 != 0)
+//                .mapToInt(Integer::intValue)
+//                .forEach(numeros::remove);
+
+        List<Integer> impares = new ArrayList<>();
+        numeros.stream()
+                .filter(s -> s % 2 != 0)
+                .mapToInt(Integer::intValue)
+                .forEach(impares::add);
+
+        System.out.println(impares);
+
+        List<Integer> pares = new ArrayList<>();
+        numeros.stream()
+                .filter(s -> s % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .forEach(pares::add);
+
+        System.out.println(pares);
+
+
+
+
+    }
+
+
+}
