36a4c9a51f10833a8c5dec216dc149743fb282da
danielhama <danielhamamura@hotmail.com>
2023-08-22 17:41:43 -0300

    Desafio3
    Signed-off-by: danielhama <danielhamamura@hotmail.com>

---
+++ src/Desafio3.java
@@ -0,0 +1,21 @@
+//Desafio 3 - Verifique se todos os números da lista são positivos:
+//       Com a ajuda da Stream API, verifique se todos os números da lista são positivos e exiba o resultado no console.
+
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Desafio3 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+       numeros.stream()
+                .filter(s -> s > 0)
+                .forEach(System.out::println);
+
+
+
+    }
+
+
+}
